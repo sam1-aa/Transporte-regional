ea5f74fb2b82ad7755839cd44988bf709dcdc24c
sam1-aa <samuelarellano1@outlook.es>
2026-04-03 18:33:35 -0600

    hicimos una muestra del recorrido que haria la ruta altepexi tehuacan centrando el mapa y visualizacion justamente al tamaño de la ruta

--- src/components/RouteMap.jsx
+++ src/components/RouteMap.jsx
@@ -1,28 +1,36 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useState } from "react";
 import { Polyline } from 'react-leaflet';
 import { TbMapPin } from "react-icons/tb";
+import data from './route.json'
+
+function AjustarVista({ ruta }) {
+    const map = useMap();
+
+    map.fitBounds(ruta);
+
+    return null;
+}
 
 function RouteMap() {
     const [unidadSeleccionada, setUnidadSeleccionada] = useState(null);
-    const ruta = [
-        [18.4616, -97.3925],
-        [18.4630, -97.3900],
-        [18.4650, -97.3880],
-        [18.4670, -97.3850]
-    ];
+    const ruta = data.routes[0].geometry.coordinates.map(([lng, lat]) => [lat, lng]);
+  
     const iconoCamion = new L.Icon({
-        iconUrl: 'https://cdn-icons-png.flaticon.com/512/61/61231.png',
+        // iconUrl: 'https://cdn-icons-png.flaticon.com/512/14703/14703667.png',
+        // iconUrl: '   https://cdn-icons-png.flaticon.com/512/2503/2503549.png',
+        // iconUrl: '   https://cdn-icons-png.flaticon.com/512/15441/15441176.png',
+        iconUrl: '   https://cdn-icons-png.flaticon.com/512/3448/3448314.png ',
         iconSize: [40, 40],       // tamaño
         iconAnchor: [20, 40],     // punto donde “toca” el mapa
     });
 
     // 🔴 AQUÍ VA unidades
     const [unidades, setUnidades] = useState([
-        { id: 1, pos: [18.462, -97.39] },
-        { id: 2, pos: [18.465, -97.38] }
+        { id: 1, pos: [18.4628, -97.3905] },
+        { id: 2, pos: [ 18.37800597571403, -97.30043649673462] }
     ]);
 
     const centro = [18.4616, -97.3925];
@@ -32,10 +40,11 @@ function RouteMap() {
         <section className="route-map-section">
             <h2 className="section-title "><TbMapPin /> Mapa en tiempo real</h2>
             <div className="map-container">
-                <MapContainer center={centro} zoom={15} className="map">
+                <MapContainer center={centro} zoom={16} className="map">
 
                     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                     <Polyline positions={ruta} />
+                    <AjustarVista ruta={ruta} />
                     {/* 🔴 AQUÍ SE USAN */}
                     {unidades.map((u) => (
                         <Marker key={u.id} position={u.pos} icon={iconoCamion} >
